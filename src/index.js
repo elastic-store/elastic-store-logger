0b6d82ac03516009bdc2f64eb194bfd4c3f012a9
ludbek <sth.srn@gmail.com>
2016-09-13 17:57:21 +0545

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+export const logger = (config) => {
+	return (actionPath, next, store) => {
+		return (state, payload) => {
+			console.log(actionPath, payload);
+			console.log("Old state:", state);
+
+			let newState = next(state, payload);
+
+			console.log("New state:", newState);
+		}
+	};
+};
