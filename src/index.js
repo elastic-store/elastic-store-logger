bb15db5a74ab1d0ecae164d0461a21a8adda3e9e
ludbek <sth.srn@gmail.com>
2016-11-04 22:25:08 +0545

    before and after states are cloned

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ export const logger = (config) => {
 	return (actionPath, next, store) => {
 		return (state, payload) => {
 			console.log(actionPath, payload);
-			console.log("Old state:", state);
+			console.log("Old state:", Object.assign({}, state));
 
 			let newState = next(state, payload);
 
-			console.log("New state:", newState);
+			console.log("New state:", Object.assign({}, newState));
 			return newState;
 		}
 	};
