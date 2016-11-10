61515c8eeac235595f83861c8dcc4c3d12d57384
ludbek <sth.srn@gmail.com>
2016-11-10 18:18:09 +0545

    diff init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+import {diff} from "deep-diff";
+
+const logDiff = (diffs) => {
+	if (!diffs) return;
+
+	diffs.forEach((diff) => {
+		console.log(diff);
+	});
+};
+
 export const logger = (config = {}) => {
 	let defaults = {
-		collapsed: true
+		collapsed: true,
+		diff: true
 	};
 
 	config = Object.assign(defaults, config);
@@ -20,13 +31,20 @@ export const logger = (config = {}) => {
 			log.payload = payload;
 			log.oldState = Object.assign({}, state);
 
+			// get new state
 			let newState = next(state, payload);
 
 			log.newState = Object.assign({}, newState);
+			log.diff = diff(log.oldState, newState);
 
 			// log
 			Object.keys(log).map((key) => {
-				console.info("%c%s:%o", "font-weight: bold", key, log[key]);
+				if (key === "diff") {
+					logDiff(log[key]);
+				}
+				else {
+					console.info("%c%s:%o", "font-weight: bold", key, log[key]);
+				}
 			});
 
 			// close group
