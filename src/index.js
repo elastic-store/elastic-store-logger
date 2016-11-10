10b176a8fbcca552dd828babf9ef45fecceeee18
ludbek <sth.srn@gmail.com>
2016-11-10 17:43:09 +0545

    updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-export const logger = (config) => {
+export const logger = (config = {}) => {
+	let defaults = {
+		collapsed: true
+	};
+
+	config = Object.assign(defaults, config);
+
 	return (actionPath, next, store) => {
 		return (state, payload) => {
-			console.log(actionPath, payload);
-			console.log("Old state:", Object.assign({}, state));
+			let log = {};
+
+			// open group
+			if (config.collapsed) {
+				console.groupCollapsed(actionPath);
+			}
+			else {
+				console.group(actionPath);
+			}
+
+			log.payload = payload;
+			log.oldState = Object.assign({}, state);
 
 			let newState = next(state, payload);
 
-			console.log("New state:", Object.assign({}, newState));
+			log.newState = Object.assign({}, newState);
+
+			// log
+			Object.keys(log).map((key) => {
+				console.info("%c%s:%o", "font-weight: bold", key, log[key]);
+			});
+
+			// close group
+			console.groupEnd(actionPath);
+
 			return newState;
 		}
 	};
