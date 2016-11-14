b7dcd0fc25138790f5fbe69df5f4ab4dfbf9177c
ludbek <sth.srn@gmail.com>
2016-11-14 10:50:32 +0545

    removed jsonpatchdiff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import {diff} from "jsondiffpatch";
-
-const logDiff = (diff) => {
-	if (!diff) return;
-	console.info("%cdiff:%O", "font-weight: bold", diff);
-};
-
 export const logger = (config = {}) => {
 	let defaults = {
-		collapsed: true,
-		diff: true
+		collapsed: true
 	};
 
 	config = Object.assign(defaults, config);
@@ -32,16 +24,10 @@ export const logger = (config = {}) => {
 			let newState = next(state, payload);
 
 			log.newState = Object.assign({}, newState);
-			log.diff = diff(log.oldState, log.newState);
 
 			// log
 			Object.keys(log).map((key) => {
-				if (key === "diff") {
-					logDiff(log[key]);
-				}
-				else {
-					console.info("%c%s:%o", "font-weight: bold", key, log[key]);
-				}
+				console.info("%c%s:%o", "font-weight: bold", key, log[key]);
 			});
 
 			// close group
